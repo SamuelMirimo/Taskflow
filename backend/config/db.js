78e9d041193803105ea4ec71f5c9dc4cefc29b3d
Samuel Mirimo <smirimo3@gmail.com>
2026-01-13 14:15:16 +0100

    refactor: remove default values from database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 
 //confguration de la connexion a la DB
 const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'taskflow_db',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 20, // Nombre maximum de connexions simultanées
     queueLimit: 0
